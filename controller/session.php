f37afcf31ca25df0cb284921a10c92429026acf2
Thomas Orgeval <orgevalthomas@gmail.com>
2020-10-07 17:38:26 +0200

    improve flash

--- controller/session.php
+++ controller/session.php
@@ -1,14 +1,27 @@
 <?php
 
-function flash(){
+function flash()
+{
     if (isset($_SESSION['Flash'])) {
         extract($_SESSION['Flash']);
         unset($_SESSION['Flash']);
-        return "<div class='alert alert-$type'>$message</div>";
+        return "<script>
+                    $(document).ready(function ()) {
+                        $('toast').toast('show')
+                    }
+                </script>
+
+                <div class='toast alert alert-$type' id='toast' style='position: absolute; top: 80px; right: 50px;'>
+                    <div class='toast-header'>
+                        <strong class='mr-auto'><i class='fa fa-book'></i> $message</strong>
+                    </div>
+                    <!--div class='toast-body'></div-->
+                </div>";
     }
 }
 
-function setFlash($message, $type = 'success') {
+function setFlash($message, $type = 'success')
+{
     $_SESSION['Flash']['message'] = $message;
     $_SESSION['Flash']['type'] = $type;
 }
