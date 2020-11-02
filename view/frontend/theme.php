64767e00abfaa53d3ecc357c9dcf6cfaf0ccb345
Thomas Orgeval <orgevalthomas@gmail.com>
2020-11-02 23:54:27 +0100

    add rainbow theme

--- view/frontend/theme.php
+++ view/frontend/theme.php
@@ -1,17 +1,17 @@
 <?php $title = 'Thèmes';
 ob_start(); ?>
 
-<h4>Voici vos thèmes :</h4>
+    <h4>Voici vos thèmes :</h4>
 
-<table>
-    <tbody>
-    <td><a class="btn btn-purple" href="index.php?p=select_theme&id=0">Défaut</a></td>
-    <?php foreach ($_POST['themes_own'] as $theme) : ?>
-        <td><a class="btn btn-purple"
-               href="index.php?p=select_theme&id=<?= $theme['slug'] ?>"><?= $theme['libelle'] ?></a></td>
-    <?php endforeach; ?>
-    </tbody>
-</table><br/>
+    <table>
+        <tbody>
+        <td><a class="btn btn-purple" href="index.php?p=select_theme&id=0">Défaut</a></td>
+        <?php foreach ($_POST['themes_own'] as $theme) : ?>
+            <td><a class="btn btn-purple"
+                   href="index.php?p=select_theme&id=<?= $theme['slug'] ?>"><?= $theme['libelle'] ?></a></td>
+        <?php endforeach; ?>
+        </tbody>
+    </table><br/>
 
 <?php if (isset($_POST['themes']) && !empty($_POST['themes'])) : ?>
     <h4>Et les autres :</h4>
@@ -20,7 +20,8 @@ ob_start(); ?>
         <tbody>
         <?php foreach ($_POST['themes'] as $theme) : ?>
             <td><a class="btn btn-outline-purple" onclick="return confirm('Voulez-vous vraiment acheter ce thème ?')"
-                   href="index.php?p=achat&id_recompense=<?= $theme['id'] ?>&page=theme"><?= $theme['libelle'] ?></a></td>
+                   href="index.php?p=achat&id_recompense=<?= $theme['id'] ?>&page=theme"><?= $theme['libelle'] ?></a>
+            </td>
         <?php endforeach; ?>
         </tbody>
     </table>
