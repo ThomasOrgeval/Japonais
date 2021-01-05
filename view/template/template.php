81eac958ecd2eb7d763cb48f9237e0a36947ee9f
Thomas Orgeval <orgevalthomas@gmail.com>
2021-01-05 11:03:23 +0100

    relativ link

--- view/template/template.php
+++ view/template/template.php
@@ -1,7 +1,12 @@
 <!DOCTYPE html>
 <html lang="fr">
 <head>
-    <base href="/japonais/">
+    <?php if ($_SERVER['HTTP_HOST'] === 'localhost') : ?>
+        <base href="/japonais/">
+    <?php else :
+        define('BASE_URL', 'https://lexiquejaponais.fr/'); ?>
+        <base href="<?= BASE_URL ?>">
+    <?php endif; ?>
     <meta name="viewport" content="width=device-width, initial-scale=1.0" charset="utf-8"/>
     <title><?= $title ?></title>
     <link rel="shortcut icon" type="image" href="./resources/svgs/sakura_login.svg">
