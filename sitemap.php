851b13f84a5a958212d6433d86ddfe526cbf30ad
Thomas Orgeval <orgevalthomas@gmail.com>
2021-05-13 21:11:20 +0200

    fix sitemap

--- sitemap.php
+++ sitemap.php
@@ -11,7 +11,7 @@ $list = [
     ['callable' => "getAllKanjis", 'prefix' => "https://lexiquejaponais.fr/kanji/", 'item' => "id", 'freq' => "never"],
     ['callable' => "getAllMusics", 'prefix' => "https://lexiquejaponais.fr/musique/", 'item' => "slug", 'freq' => "never"]
 ];
-regre
+
 foreach ($list as $item) {
     $words = call_user_func($item['callable']);
     foreach ($words as $word) {
